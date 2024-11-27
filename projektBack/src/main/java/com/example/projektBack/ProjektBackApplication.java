2380002bb399387ac1fc38f4396d9ae902ecd05e
Rostyslav Boklach <sana22_brv@student.ztu.edu.ua>
2024-11-27 16:21:33 +0100

    first commit

---
+++ projektBack/src/main/java/com/example/projektBack/ProjektBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektBackApplication.class, args);
+	}
+
+}
---
+++ projektBack/src/test/java/com/example/projektBack/ProjektBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projektBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
